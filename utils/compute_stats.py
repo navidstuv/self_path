874a4afb89bbc69fbb71d6dd5c4f61989a488cc8
navidstuv <navid.alemi@gmail.com>
2020-05-05 23:15:01 +0100

    Update compute_stat.py

--- utils/compute_stats.py
+++ utils/compute_stats.py
@@ -1,6 +1,6 @@
 from utils import stats
 import numpy as np
-
-pred = np.load('../pred_test_main1.npy')
-true = np.load('../true_test_main1.npy')
-stats(pred, true, 0.4621)
+from configs.configs import config
+pred = np.load('../pred_'+config.mode+'main3.npy')
+true = np.load('../true_'+config.mode+'main3.npy')
+stats(pred, true,0.5)
