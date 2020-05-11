3bfa850f25bf1c3e9e90957368eb021a813e92f4
navidstuv <navid.alemi@gmail.com>
2020-05-11 22:02:47 +0100

    config changed

--- configs/configs.py
+++ configs/configs.py
@@ -2,25 +2,25 @@ import os
 class DefaultConfigs(object):
 
 
-    mode = 'test'
+    mode = 'train'
     encoder_name = 'resnet50'
     pretrained = False
     stain_normalized = True
-    augmentation = True
+    augmentation = False
     exp_name = ' cam-oscc'
-    task_names = ['main_task','magnification', 'domain_classifier']#['main_task', 'magnification', 'stain', 'domain_classifier']
+    task_names = ['main_task', 'magnification']#['main_task', 'magnification', 'gamma', 'domain_classifier']
     aux_task_names =task_names[1:]
     tasks = {'magnification': {'type': 'classification', 'n_classes': 3},
              'main_task': {'type': 'classification', 'n_classes': 2},
-             'stain': {'type': 'classification', 'n_classes': 2},
+             'gamma': {'type': 'regression', 'n_classes': None},
              'domain_classifier': {'type': 'classification', 'n_classes': 2}}
     loss_weight = {'magnification': 0.2, 'domain_classifier':0.2, 'main_task': 1, 'stain':0.2}
 
-    log_dir = './main_normalized2/logs'
-    cache_dir = './main_normalized2/cache'
-    model_dir = './main_normalized2/model'
-    best_model_dir = './main_normalized2/best_model'
-    training_resume = ''
+    log_dir = './main_magnification_normalized1/logs'
+    cache_dir = './main_magnification_normalized1/cache'
+    model_dir = './main_magnification_normalized1/model'
+    best_model_dir = './main_magnification_normalized1/best_model'
+    training_resume = 'model_last.pth'
     training_num_print_epoch = 20
 
 
@@ -46,15 +46,15 @@ class DefaultConfigs(object):
     # base_data_path_unlabel_new = 'G://test_camelyon'
     pickle_path_test = './pickle_files/test.pickle'
     budget_test = 'test1'
-    testing_model ='/media/navid/HDD1/Back_up/experiments_log/domain_adoptation/main_mag_dom_normalized3/best_model/model_best.pth'
+    testing_model ='/media/navid/HDD1/Back_up/experiments_log/domain_adoptation/main_dom3_1/best_model/model_best.pth'
 
-    save_output = True
+    save_output = False
     eval_batch_size = 128
     test_batch_size = 128
 
 
 
-    random_seed = 33
+    random_seed = 22
     num_epochs = 100
     gpus = [0,1]
     lr =  0.001
