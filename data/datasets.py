e2931c279f37415f6f04b216ce66a9b31cfff74f
navidstuv <navid.alemi@gmail.com>
2020-05-11 21:47:27 +0100

    data loade clean up

--- data/datasets.py
+++ data/datasets.py
@@ -39,7 +39,7 @@ class Histodata(Dataset):
         self.augment = augment
         if config.stain_normalized:
             self.n = stainNorm_Reinhard.Normalizer()
-            i1 = cv2.imread('../data/source.png')
+            i1 = cv2.imread('data/source.png')
             i1 = cv2.cvtColor(i1, cv2.COLOR_BGR2RGB)
             self.n.fit(i1)
         normal_label = []
@@ -127,29 +127,14 @@ class Histodata_unlabel_domain_adopt(Dataset):
             i1 = cv2.imread('data/source.png')
             i1 = cv2.cvtColor(i1, cv2.COLOR_BGR2RGB)
             self.n.fit(i1)
-        normal_label = []
-        tumour_label = []
-        normal_path = []
-        tumour_path = []
         normal_path_all = []
         tumour_path_all = []
         normal_label_all = []
         tumour_label_all = []
-
-
         with open(pickle_path, 'rb') as f:
             data_budget = pickle.load(f)
-        for image_name in data_budget[budget]['patches']['Normal']:
-            normal_path.append(os.path.join('Normal', image_name))
-            normal_label.append(0)
-        for image_name in data_budget[budget]['patches']['Tumour']:
-            tumour_path.append(os.path.join('Tumour', image_name))
-            tumour_label.append(1)
-        self.imgs = np.append(normal_path,tumour_path)
-        self.labels = np.append(normal_label,tumour_label)
 
         if self.unlabeled ==True:
-
             for image_name in data_budget[config.budget_unlabel]['patches']['Normal']:
                 normal_path_all.append(os.path.join('Normal', image_name))
                 normal_label_all.append(0)
@@ -157,86 +142,59 @@ class Histodata_unlabel_domain_adopt(Dataset):
                 tumour_path_all.append(os.path.join('Tumour', image_name))
                 tumour_label_all.append(1)
             self.imgs_unlabel = np.append(normal_path_all, tumour_path_all)
-            # self.imgs_unlabel = list(set(self.imgs_all) - set(self.imgs))
 
     def __getitem__(self,index):
-        if self.unlabeled==True:
-            filename = self.imgs_unlabel[index]
-            img = cv2.imread(os.path.join(self.path, filename))
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        filename = self.imgs_unlabel[index]
+        img = cv2.imread(os.path.join(self.path, filename))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if config.stain_normalized:
+            img = self.n.transform(img)
+        if self.augment:
+            img = self.augment(img.shape)(image=img)
+            img = img['image']
+        # resize image
+        width = int(img.shape[1] * 25 / 100)
+        height = int(img.shape[0] * 25 / 100)
+        dim = (width, height)
+        main_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+        if 'magnification' in config.task_names:
+            mag = np.random.choice(['40x', '20x', '10x'], 1)
+            if mag=='40x':
+                aux_image_mag = center_crop(img, 128, 128)
+                aux_label_mag = 0
+            if mag=='20x':
+                aux_image_mag = center_crop(img, 256, 256)
+                aux_image_mag = cv2.resize(aux_image_mag, (128, 128), interpolation=cv2.INTER_AREA)
+                aux_label_mag = 1
+            if mag=='10x':
+                aux_image_mag = main_img
+                aux_label_mag = 2
 
-            # resize image
-            width = int(img.shape[1] * 25 / 100)
-            height = int(img.shape[0] * 25 / 100)
-            dim = (width, height)
-            main_img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-            if self.augment:
-                main_img = self.augment(main_img.shape)(image=main_img)
-                main_img = main_img['image']
-            if 'magnification' in config.task_names:
-                mag = np.random.choice(['40x', '20x', '10x'], 1)
-                if mag=='40x':
-                    aux_image_mag = center_crop(img, 128, 128)
-                    aux_label_mag = 0
-                if mag=='20x':
-                    aux_image_mag = center_crop(img, 256, 256)
-                    aux_image_mag = cv2.resize(aux_image_mag, (128, 128), interpolation=cv2.INTER_AREA)
-                    aux_label_mag = 1
-                if mag=='10x':
-                    aux_image_mag = main_img
-                    aux_label_mag = 2
-                if self.augment:
-                    aux_image_mag = self.augment(aux_image_mag.shape)(image=aux_image_mag)
-                    aux_image_mag = aux_image_mag['image']
+            aux_image_mag = preprocess_input(aux_image_mag.astype(np.float32))
+            aux_image_mag = torch.from_numpy(aux_image_mag).float()
+            aux_image_mag = aux_image_mag.permute(2, 0, 1)
 
-                if config.stain_normalized:
-                    aux_image_mag = self.n.transform(aux_image_mag)
-                aux_image_mag = preprocess_input(aux_image_mag.astype(np.float32))
-                aux_image_mag = torch.from_numpy(aux_image_mag).float()
-                aux_image_mag = aux_image_mag.permute(2, 0, 1)
+        if 'gamma' in config.task_names:
+            gamma = np.random.randint(50, 150) / 100.0
+            # print(gamma)
+            aux_image_gamma = F_aug.gamma_transform(main_img, gamma=gamma)
+            aux_label_gamma = gamma
+            aux_image_gamma = preprocess_input(aux_image_gamma.astype(np.float32))
+            aux_image_gamma = torch.from_numpy(aux_image_gamma).float()
+            aux_image_gamma = aux_image_gamma.permute(2, 0, 1)
+            aux_label_gamma = torch.from_numpy(np.array(aux_label_gamma)).float()
+        main_img = preprocess_input (main_img.astype(np.float32))
+        main_img = torch.from_numpy(main_img).float()
+        main_img = main_img.permute(2, 0, 1)
 
-            if 'gamma' in config.task_names:
-                gamma = np.random.randint(50, 150) / 100.0
-                # print(gamma)
-                aux_image_gamma = F_aug.gamma_transform(main_img, gamma=gamma)
-                aux_label_gamma = gamma
-                aux_image_gamma = preprocess_input(aux_image_gamma.astype(np.float32))
-                aux_image_gamma = torch.from_numpy(aux_image_gamma).float()
-                aux_image_gamma = aux_image_gamma.permute(2, 0, 1)
-                aux_label_gamma = torch.from_numpy(np.array(aux_label_gamma)).float()
-            if config.stain_normalized:
-                main_img = self.n.transform(main_img)
-            main_img = preprocess_input (main_img.astype(np.float32))
-            main_img = torch.from_numpy(main_img).float()
-            main_img = main_img.permute(2, 0, 1)
-
-            if 'magnification' in config.task_names and 'stain' not in config.task_names:
-                return main_img, -1, aux_image_mag, aux_label_mag, -1, -1
-            elif 'stain' in config.task_names and 'magnification' not in config.task_names:
-                return main_img, -1, -1, -1, aux_image_gamma, aux_label_gamma
-            elif 'stain' in config.task_names and 'magnification' in config.task_names:
-                return main_img, -1, aux_image_mag, aux_label_mag, aux_image_gamma, aux_label_gamma
-            else:
-                return main_img, -1, -1, -1, -1, -1
+        if 'magnification' in config.task_names and 'stain' not in config.task_names:
+            return main_img, -1, aux_image_mag, aux_label_mag, -1, -1
+        elif 'stain' in config.task_names and 'magnification' not in config.task_names:
+            return main_img, -1, -1, -1, aux_image_gamma, aux_label_gamma
+        elif 'stain' in config.task_names and 'magnification' in config.task_names:
+            return main_img, -1, aux_image_mag, aux_label_mag, aux_image_gamma, aux_label_gamma
         else:
-            filename = self.imgs[index]
-            img = cv2.imread(os.path.join(self.path, filename))
-            label = self.labels[index]
-            img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-
-            # resize image
-            width = int(img.shape[1] * 25 / 100)
-            height = int(img.shape[0] * 25 / 100)
-            dim = (width, height)
-            img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-            if self.augment:
-                img = self.augment(img.shape)(image=img)
-                img = img['image']
-            img = preprocess_input (img.astype(np.float32))
-            img = torch.from_numpy(img).float()
-            img = img.permute(2, 0, 1)
-            label = torch.from_numpy(np.array(label)).long()
-            return img, label
+            return main_img, -1, -1, -1, -1, -1
 
     def __len__(self):
         if self.unlabeled==True:
@@ -280,8 +238,10 @@ if __name__=='__main__':
 
     def worker_init_fn(worker_id):
         np.random.seed(np.random.get_state()[1][0] + worker_id)
-    lab_train_generator = Histodata(config.base_data_path_unlabel, '../pickle_files/training.pickle', 'training1', unlabeled=False , augment = get_medium_augmentations)
-    src_loader = DataLoader(lab_train_generator, batch_size=config.src_batch_size, shuffle=True, num_workers=20,
+    lab_train_generator = Histodata(config.base_data_path_unlabel, '../pickle_files/training.pickle', 'training1', unlabeled=False , augment = False)
+    unlab_train_generator = Histodata_unlabel_domain_adopt(config.base_data_path_unlabel, '../pickle_files/training.pickle',
+                                                           config.budget_unlabel, unlabeled = True, augment= False)
+    src_loader = DataLoader(unlab_train_generator, batch_size=config.src_batch_size, shuffle=True, num_workers=20,
                             pin_memory=True, worker_init_fn=worker_init_fn)
     for it, src in enumerate(src_loader):
         img,lbl, aux_img,aux_lbs,_,_ = src
