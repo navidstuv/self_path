0a390595f7aa23fab872e57cf0e2a78659e00473
navidstuv <navid.alemi@gmail.com>
2020-07-17 16:39:45 +0100

    small fix

--- configs/configs.py
+++ configs/configs.py
@@ -2,13 +2,13 @@ import os
 class DefaultConfigs(object):
 
 
-    mode = 'test'
+    mode = 'train'
     encoder_name = 'resnet50'
     pretrained = False
     stain_normalized = True
     augmentation = True
     exp_name = ' cam-oscc'
-    task_names = ['main_task', 'jigsaw']#['main_task', 'magnification', 'jigsaw', 'domain_classifier']
+    task_names = ['main_task']#['main_task', 'magnification', 'jigsaw', 'domain_classifier']
     aux_task_names =task_names[1:]
     tasks = {'magnification': {'type': 'classification', 'n_classes': 3},
              'main_task': {'type': 'classification', 'n_classes': 2},
@@ -18,10 +18,10 @@ class DefaultConfigs(object):
              }
     loss_weight = {'magnification': 1, 'domain_classifier': 1, 'main_task': 1, 'jigsaw': 1, 'hematoxylin': 1}
 
-    log_dir = './test_hematoxylin2/logs'
-    cache_dir = './test_hematoxylin2/cache'
-    model_dir = './test_hematoxylin2/model'
-    best_model_dir = './test_hematoxylin2/best_model'
+    log_dir = './test_main_semi0.2/logs'
+    cache_dir = './test_main_semi0.2/cache'
+    model_dir = './test_main_semi0.2/model'
+    best_model_dir = './test_main_semi0.2/best_model'
 
     training_resume = ''
     training_num_print_epoch = 20
@@ -30,27 +30,27 @@ class DefaultConfigs(object):
     #source domain
     src_batch_size = 64
     # base_data_path = '/media/navid/SeagateBackupPlusDrive/512allcamelyon'
-    base_data_path = '/media/navid/SeagateBackupPlusDrive/512allcamelyon'
-    pickle_path = 'pickle_files/training_cam.pickle'
-    budget = 'training_cam1'
+    base_data_path = '/media/navid/SeagateBackupPlusDrive/512all'
+    pickle_path = 'pickle_files/training_balanced.pickle'
+    budget = 'training0.2'
 
     #target domain
     tar_batch_size = 64
     # base_data_path_unlabel = '/media/navid/SeagateBackupPlusDrive/512all'
     base_data_path_unlabel = '/media/navid/SeagateBackupPlusDrive/512all'
-    pickle_path_unlabel= './pickle_files/training.pickle'
+    pickle_path_unlabel= 'pickle_files/training_balanced.pickle'
     budget_unlabel = 'training1'
 
     # validation
-    pickle_path_valid = './pickle_files/validation.pickle'
+    pickle_path_valid = './pickle_files/validation_balanced.pickle'
     budget_valid = 'validation1'
 
     # test
     # base_data_path_unlabel_new = 'G://test_camelyon'
-    pickle_path_test = './pickle_files/test.pickle'
+    pickle_path_test = './pickle_files/test_balanced.pickle'
     budget_test = 'test1'
 
-    testing_model ='./test_hematoxylin/best_model/model_best.pth'
+    testing_model ='./test_magnification_semi0.05/best_model/model_best.pth'
 
 
     save_output = True
@@ -61,7 +61,7 @@ class DefaultConfigs(object):
 
     random_seed = 33
     num_epochs = 100
-    gpus = [0,1]
+    gpus = [0]
     lr =  0.001
 
 
