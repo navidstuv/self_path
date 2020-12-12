59d4f67649072cd1c2ac2a087e83f4780de33c4c
navidstuv <navid.alemi@gmail.com>
2020-12-12 21:02:00 +0100

    jigsaw puzzle modified

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,8 @@
 from albumentations.augmentations.transforms import Resize
 import numpy as np
 import cv2
+import random
+import itertools
 
 def center_crop(img, crop_height, crop_width):
     height, width = img.shape[:2]
@@ -31,88 +33,19 @@ def jigsaw_res(big_image):
 
     jig = np.zeros((128, 128, 3))
     img_40 = center_crop(big_image, 64, 64)
-
     img_20 = center_crop(big_image, 128, 128)
     img_20 = cv2.resize(img_20, (64, 64), interpolation=cv2.INTER_CUBIC)
-
     img_10 = center_crop(big_image, 256, 256)
     img_10 = cv2.resize(img_10, (64, 64), interpolation=cv2.INTER_CUBIC)
-
     img_5 = cv2.resize(big_image, (64, 64))
-
-    order = np.random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9','10','11','12'], 1)
-    if order=='1':
-        jig[:64, :64, :] = img_40
-        jig[64:, 64:, :] = img_20
-        jig[:64, 64:, :] = img_10
-        jig[64:, :64, :] = img_5
-        jig_lbl = 0
-    elif order=='2':
-        jig[:64, :64, :] = img_40
-        jig[64:, 64:, :] = img_10
-        jig[:64, 64:, :] = img_20
-        jig[64:, :64, :] = img_5
-        jig_lbl = 1
-    elif order=='3':
-        jig[:64, :64, :] = img_40
-        jig[64:, 64:, :] = img_5
-        jig[:64, 64:, :] = img_20
-        jig[64:, :64, :] = img_10
-        jig_lbl = 2
-    elif order=='4':
-        jig[:64, :64, :] = img_5
-        jig[64:, 64:, :] = img_40
-        jig[:64, 64:, :] = img_20
-        jig[64:, :64, :] = img_10
-        jig_lbl = 3
-    elif order=='5':
-        jig[:64, :64, :] = img_10
-        jig[64:, 64:, :] = img_5
-        jig[:64, 64:, :] = img_20
-        jig[64:, :64, :] = img_40
-        jig_lbl = 4
-    elif order=='6':
-        jig[:64, :64, :] = img_20
-        jig[64:, 64:, :] = img_5
-        jig[:64, 64:, :] = img_10
-        jig[64:, :64, :] = img_40
-        jig_lbl = 5
-    elif order=='7':
-        jig[:64, :64, :] = img_20
-        jig[64:, 64:, :] = img_10
-        jig[:64, 64:, :] = img_40
-        jig[64:, :64, :] = img_5
-        jig_lbl = 6
-    elif order=='8':
-        jig[:64, :64, :] = img_10
-        jig[64:, 64:, :] = img_40
-        jig[:64, 64:, :] = img_5
-        jig[64:, :64, :] = img_20
-        jig_lbl = 7
-    elif order=='9':
-        jig[:64, :64, :] = img_10
-        jig[64:, 64:, :] = img_5
-        jig[:64, 64:, :] = img_20
-        jig[64:, :64, :] = img_40
-        jig_lbl = 8
-    elif order=='10':
-        jig[:64, :64, :] = img_20
-        jig[64:, 64:, :] = img_10
-        jig[:64, 64:, :] = img_5
-        jig[64:, :64, :] = img_40
-        jig_lbl = 9
-    elif order=='11':
-        jig[:64, :64, :] = img_40
-        jig[64:, 64:, :] = img_20
-        jig[:64, 64:, :] = img_5
-        jig[64:, :64, :] = img_10
-        jig_lbl = 10
-    elif order=='12':
-        jig[:64, :64, :] = img_5
-        jig[64:, 64:, :] = img_10
-        jig[:64, 64:, :] = img_40
-        jig[64:, :64, :] = img_20
-        jig_lbl = 11
+    list_imgs = [img_5, img_10, img_20, img_40]
+    all_possible_permutations = list(itertool.permutations(list_imgs))
+    order = random.sample(list(range(23)), 1)
+    jig[:64, :64, :] = all_possible_permutations[order][0]
+    jig[64:, 64:, :] = all_possible_permutations[order][1]
+    jig[:64, 64:, :] = all_possible_permutations[order][2]
+    jig[64:, :64, :] = all_possible_permutations[order][3]
+    jig_lbl = order
     return jig, jig_lbl
 
 
